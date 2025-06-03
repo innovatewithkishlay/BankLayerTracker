9d17810465296f8d7b117483d7ab93ce013b181b
kishlay <kishlay141@gmail.com>
2025-06-04 03:17:57 +0530

    wrote connectDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
