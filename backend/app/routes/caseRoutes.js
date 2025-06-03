e4d63a69b354032f994006025f07926286fc8be2
kishlay <kishlay141@gmail.com>
2025-06-03 15:45:53 +0530

    made the plug-in engine

--- backend/app/routes/caseRoutes.js
+++ backend/app/routes/caseRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { processSingleCase } = require("../controllers/caseController");
 const upload = require("../../config/multer");
 
+// (Plug-in Engine)
+
 router.post("/upload/plugin", upload.single("csvFile"), processSingleCase);
 
 module.exports = router;
