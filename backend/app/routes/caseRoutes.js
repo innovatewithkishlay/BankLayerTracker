2b8b0731593bd51ed1126615933035eddfcd435f
kishlay <kishlay141@gmail.com>
2025-06-03 15:44:31 +0530

    required processSingleCase and multer

--- backend/app/routes/caseRoutes.js
+++ backend/app/routes/caseRoutes.js
@@ -1,2 +1,4 @@
 const express = require("express");
 const router = express.Router();
+const { processSingleCase } = require("../controllers/caseController");
+const upload = require("../../config/multer");
