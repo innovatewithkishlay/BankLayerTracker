7f738fd08637ba66a88d687cc35c83648310a781
kishlay <kishlay141@gmail.com>
2025-06-03 15:23:11 +0530

    completed making the transactionSchema

--- backend/app/models/Transaction.js
+++ backend/app/models/Transaction.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+const transactionSchema = new mongoose.Schema({
+  fromAccount: { type: String, required: true },
+  toAccount: { type: String, required: true },
+  amount: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  description: { type: String },
+  caseId: { type: mongoose.Schema.Types.ObjectId, ref: "Case" },
+  isSuspicious: { type: Boolean, default: false },
+});
