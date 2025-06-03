8471799172a418d0acecbb834be9af294626029f
kishlay <kishlay141@gmail.com>
2025-06-03 15:20:23 +0530

    completing the Account.js

--- backend/app/models/Account.js
+++ backend/app/models/Account.js
@@ -1,1 +1,15 @@
 const mongoose = require("mongoose");
+const accountSchema = new mongoose.Schema({
+  accountNumber: { type: String, required: true, unique: true },
+  accountHolder: { type: String, required: true },
+  accountType: { type: String, enum: ["personal", "business"], required: true },
+  metadata: {
+    mobile: { type: String },
+    ipAddress: { type: String },
+    email: { type: String },
+  },
+  caseId: { type: mongoose.Schema.Types.ObjectId, ref: "Case" },
+  isFlagged: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("Account", accountSchema);
