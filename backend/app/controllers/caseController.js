e55e923493f7bd6e15090abe5197dae0562acab3
kishlay <kishlay141@gmail.com>
2025-06-03 15:40:17 +0530

    required detectAnomalies which will later be added on

--- backend/app/controllers/caseController.js
+++ backend/app/controllers/caseController.js
@@ -2,3 +2,4 @@ const Case = require("../models/Case");
 const Account = require("../models/Account");
 const Transaction = require("../models/Transaction");
 const parseCSV = require("../utils/csvParser");
+const { detectAnomalies } = require("../utils/anomalyDetector");
