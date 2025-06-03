fd9976d6ccb45a308bbd3b80811af8d319452b02
kishlay <kishlay141@gmail.com>
2025-06-04 03:43:23 +0530

    made anomaly_thresholds

--- backend/app/utils/anomalyDetector.js
+++ backend/app/utils/anomalyDetector.js
@@ -1,2 +1,9 @@
 const Transaction = require("../models/Transaction");
 const Account = require("../models/Account");
+
+const ANOMALY_THRESHOLDS = {
+  HIGH_VALUE: 50000, // $50,000+ transactions
+  RAPID_SUCCESSIVE: { count: 5, minutes: 10 }, // 5+ transactions in 10 mins
+  STRUCTURING_LIMIT: 10000, // Multiple just-below-$10k transactions
+  RISKY_COUNTRIES: ["XX", "YY", "ZZ"], // High-risk country codes
+};
