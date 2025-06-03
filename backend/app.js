7eab69342236570096a8911447c7d0820e872cd8
kishlay <kishlay141@gmail.com>
2025-06-04 03:35:55 +0530

    setuped middlewares

--- backend/app.js
+++ backend/app.js
@@ -3,3 +3,10 @@ const express = require("express");
 const { helmet, limiter, cors, corsOptions } = require("./config/security");
 const connectDB = require("./config/db");
 const caseRoutes = require("./app/routes/caseRoutes");
+const app = express();
+
+// Security Middlewares
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(limiter);
+app.use(express.json());
