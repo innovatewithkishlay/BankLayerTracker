9eaa37433df86a7c4bddad8479b1c946ee3f28a3
kishlay <kishlay141@gmail.com>
2025-06-04 03:37:18 +0530

    starting the server now

--- backend/app.js
+++ backend/app.js
@@ -22,3 +22,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Internal server error!" });
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
