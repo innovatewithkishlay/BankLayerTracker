a72403aa6b0f56077cc61ac174323d4329b98d6d
kishlay <kishlay141@gmail.com>
2025-06-03 15:30:03 +0530

    made parseCSV

--- backend/app/utils/csvParser.js
+++ backend/app/utils/csvParser.js
@@ -1,1 +1,11 @@
 const csv = require("csv-parser");
+const parseCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", () => resolve(results))
+      .on("error", (err) => reject(err));
+  });
+};
