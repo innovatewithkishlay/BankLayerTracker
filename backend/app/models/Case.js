16b4df1232bee0fc7fc2a9c0ec6366a34d742203
kishlay <kishlay141@gmail.com>
2025-06-03 15:25:53 +0530

    wrote CaseSchema

--- backend/app/models/Case.js
+++ backend/app/models/Case.js
@@ -1,1 +1,8 @@
 const mongoose = require("mongoose");
+const caseSchema = new mongoose.Schema({
+  caseId: { type: String, required: true, unique: true },
+  description: { type: String },
+  accounts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Account" }],
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }],
+  linkedCases: [{ type: mongoose.Schema.Types.ObjectId, ref: "Case" }],
+});
