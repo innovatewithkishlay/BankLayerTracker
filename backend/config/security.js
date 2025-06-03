63d92bbe58dcf68242fc17bca3ae7c0599fb48b1
kishlay <kishlay141@gmail.com>
2025-06-04 03:32:29 +0530

    made a limiter requests

--- backend/config/security.js
+++ backend/config/security.js
@@ -1,3 +1,10 @@
 const helmet = require("helmet");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
