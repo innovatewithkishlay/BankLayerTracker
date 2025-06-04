9f9a55f59a91be60d52805a5e72871ac5468e458
kishlay <kishlay141@gmail.com>
2025-06-04 11:56:17 +0530

    made controller for deleting case

--- backend/app/controllers/caseController.js
+++ backend/app/controllers/caseController.js
@@ -67,3 +67,17 @@ exports.getCaseById = async (req, res) => {
     res.status(500).json({ error: "Server error: " + err.message });
   }
 };
+
+exports.deleteCase = async (req, res) => {
+  try {
+    const deletedCase = await Case.findByIdAndDelete(req.params.caseId);
+    if (!deletedCase) return res.status(404).json({ error: "Case not found" });
+
+    await Account.deleteMany({ caseId: deletedCase._id });
+    await Transaction.deleteMany({ caseId: deletedCase._id });
+
+    res.status(200).json({ message: "Case deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Server error: " + err.message });
+  }
+};
