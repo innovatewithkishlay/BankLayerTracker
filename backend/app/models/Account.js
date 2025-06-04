929e7471df37e631e16eb88bb23d7c559ae543d9
kishlay <kishlay141@gmail.com>
2025-06-04 15:38:49 +0530

    updated the Account schema to eliminate the duplicay issue

--- backend/app/models/Account.js
+++ backend/app/models/Account.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose");
+
 const accountSchema = new mongoose.Schema({
-  accountNumber: { type: String, required: true, unique: true },
-  accountHolder: { type: String, required: true },
-  accountType: { type: String, enum: ["personal", "business"], required: true },
+  accountNumber: {
+    type: String,
+    required: true,
+  },
+  accountHolder: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    enum: ["personal", "business"],
+    required: true,
+  },
   metadata: {
     mobile: { type: String },
     ipAddress: { type: String },
     email: { type: String },
   },
-  caseId: { type: mongoose.Schema.Types.ObjectId, ref: "Case" },
-  isFlagged: { type: Boolean, default: false },
+  caseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Case",
+    required: true,
+  },
+  isFlagged: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+accountSchema.index({ accountNumber: 1, caseId: 1 }, { unique: true });
 
 module.exports = mongoose.model("Account", accountSchema);
