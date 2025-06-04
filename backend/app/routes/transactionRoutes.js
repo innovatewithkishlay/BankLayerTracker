fe67988da29d1992a56db7a79ce7138c7df8d603
kishlay <kishlay141@gmail.com>
2025-06-04 11:58:59 +0530

    write routes for transaction.js

--- backend/app/routes/transactionRoutes.js
+++ backend/app/routes/transactionRoutes.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getTransaction,
+  flagTransaction,
+} = require("../controllers/transactionController");
+
+router.get("/:transactionId", getTransaction);
+router.patch("/:transactionId/flag", flagTransaction);
+
+module.exports = router;
