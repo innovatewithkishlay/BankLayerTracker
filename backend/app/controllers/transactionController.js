59bb908b4748bcc029bb6a1097b04e87f51c5a40
kishlay <kishlay141@gmail.com>
2025-06-04 12:02:18 +0530

    made getTransaction

--- backend/app/controllers/transactionController.js
+++ backend/app/controllers/transactionController.js
@@ -1,1 +1,11 @@
 const Transaction = require("../models/Transaction");
+exports.getTransaction = async (req, res) => {
+  try {
+    const transaction = await Transaction.findById(req.params.transactionId);
+    if (!transaction)
+      return res.status(404).json({ error: "Transaction not found" });
+    res.status(200).json(transaction);
+  } catch (err) {
+    res.status(500).json({ error: "Server error: " + err.message });
+  }
+};
