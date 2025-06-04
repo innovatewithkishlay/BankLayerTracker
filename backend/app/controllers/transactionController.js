50335f4c42df015ea34fa90eda04894527a644d2
kishlay <kishlay141@gmail.com>
2025-06-04 12:03:24 +0530

    made flagTransaction

--- backend/app/controllers/transactionController.js
+++ backend/app/controllers/transactionController.js
@@ -9,3 +9,17 @@ exports.getTransaction = async (req, res) => {
     res.status(500).json({ error: "Server error: " + err.message });
   }
 };
+exports.flagTransaction = async (req, res) => {
+  try {
+    const transaction = await Transaction.findByIdAndUpdate(
+      req.params.transactionId,
+      { isSuspicious: req.body.flag },
+      { new: true }
+    );
+    if (!transaction)
+      return res.status(404).json({ error: "Transaction not found" });
+    res.status(200).json(transaction);
+  } catch (err) {
+    res.status(500).json({ error: "Server error: " + err.message });
+  }
+};
