3e3db57074bded6e3166a40df8b787d4513ff567
kishlay <kishlay141@gmail.com>
2025-06-05 11:01:10 +0530

    updated the AnomalySparkline with @nivo/line

--- frontend/src/components/DataDisplay/AnomalySparkline.tsx
+++ frontend/src/components/DataDisplay/AnomalySparkline.tsx
@@ -1,4 +1,4 @@
-import { Sparkline } from "@nivo/sparkline";
+import { ResponsiveLine } from "@nivo/line";
 
 interface AnomalySparklineProps {
   data: number[];
@@ -10,14 +10,35 @@ export const AnomalySparkline = ({
   data,
   width = 128,
   height = 48,
-}: AnomalySparklineProps) => (
-  <div style={{ width, height }}>
-    <Sparkline
-      data={data}
-      width={width}
-      height={height}
-      curve="natural"
-      colors={["#00ff9d"]}
-    />
-  </div>
-);
+}: AnomalySparklineProps) => {
+  const formattedData = [
+    {
+      id: "anomalies",
+      data: data.map((y, index) => ({ x: index, y })),
+    },
+  ];
+
+  return (
+    <div style={{ width, height }}>
+      <ResponsiveLine
+        data={formattedData}
+        margin={{ top: 5, right: 5, bottom: 5, left: 5 }}
+        xScale={{ type: "linear" }}
+        yScale={{ type: "linear", min: "auto", max: "auto" }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={null}
+        axisLeft={null}
+        enableGridX={false}
+        enableGridY={false}
+        colors={["#00ff9d"]}
+        lineWidth={2}
+        pointSize={0}
+        useMesh={false}
+        enableArea={true}
+        areaOpacity={0.15}
+        isInteractive={false}
+      />
+    </div>
+  );
+};
