d63c3152fb26c5e838666c82a23e1cf945f9983c
kishlay <kishlay141@gmail.com>
2025-06-05 10:53:12 +0530

    modified AnomalySparkline

--- frontend/src/components/DataDisplay/AnomalySparkline.tsx
+++ frontend/src/components/DataDisplay/AnomalySparkline.tsx
@@ -1,1 +1,23 @@
 import { Sparkline } from "@nivo/sparkline";
+
+interface AnomalySparklineProps {
+  data: number[];
+  width?: number;
+  height?: number;
+}
+
+export const AnomalySparkline = ({
+  data,
+  width = 128,
+  height = 48,
+}: AnomalySparklineProps) => (
+  <div style={{ width, height }}>
+    <Sparkline
+      data={data}
+      width={width}
+      height={height}
+      curve="natural"
+      colors={["#00ff9d"]}
+    />
+  </div>
+);
