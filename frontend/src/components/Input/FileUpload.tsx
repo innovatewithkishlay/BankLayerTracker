2453f5861e8f40b5c2cd49ca27319d9a30efddfa
kishlay <kishlay141@gmail.com>
2025-06-05 10:28:14 +0530

    started with FileUpload

---
+++ frontend/src/components/Input/FileUpload.tsx
@@ -0,0 +1,4 @@
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { FiUploadCloud } from "react-icons/fi";
+import { motion } from "framer-motion";
