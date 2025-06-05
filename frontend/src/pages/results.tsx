166cfecd87ce0e40c1acb5ce282a5ee2e6915a3f
kishlay <kishlay141@gmail.com>
2025-06-05 11:57:18 +0530

    started with the Result page

---
+++ frontend/src/pages/results.tsx
@@ -0,0 +1,5 @@
+import { useParams } from "react-router-dom";
+import { useAML } from "../hooks/useApi";
+import { useEffect, useState } from "react";
+import { NetworkGraph } from "../components/DataDisplay/NetworkGraph";
+import { AnomalyTable } from "../components/DataDisplay/AnomalyTable";
