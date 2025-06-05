42d5774b2a9afd3a12f097ce6a65427799ef4380
kishlay <kishlay141@gmail.com>
2025-06-05 10:26:52 +0530

    modified the useApi

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -2,3 +2,24 @@ import axios from "axios";
 import type { CaseComparison, Transaction } from "../types/apiTypes";
 
 const API_URL = "http://localhost:5000/api";
+
+export const useAML = () => {
+  const uploadCase = async (file: File) => {
+    const formData = new FormData();
+    formData.append("csvFile", file);
+    const { data } = await axios.post<{ caseId: string }>(
+      `${API_URL}/cases/upload/plugin`,
+      formData
+    );
+    return data.caseId;
+  };
+
+  const compareCases = async (case1Id: string, case2Id: string) => {
+    const { data } = await axios.get<CaseComparison>(
+      `${API_URL}/cases/compare/${case1Id}/${case2Id}`
+    );
+    return data;
+  };
+
+  return { uploadCase, compareCases };
+};
