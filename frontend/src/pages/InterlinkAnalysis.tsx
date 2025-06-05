a336e782a45442b8442478f93ef30459e7d00f5c
kishlay <kishlay141@gmail.com>
2025-06-05 11:07:33 +0530

    started with InterlinkAnalysis

---
+++ frontend/src/pages/InterlinkAnalysis.tsx
@@ -0,0 +1,5 @@
+import { useAML } from "../hooks/useApi";
+import { FileUpload } from "../components/Input/FileUpload";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
