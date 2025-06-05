9cad7a459cc7311ddcd9d50bd08d9bafadac61fa
kishlay <kishlay141@gmail.com>
2025-06-05 13:05:40 +0530

    final change to react-simple-maps.d.ts

--- frontend/src/react-simple-maps.d.ts
+++ frontend/src/react-simple-maps.d.ts
@@ -12,11 +12,32 @@ declare module "react-simple-maps" {
       hover?: React.CSSProperties;
       pressed?: React.CSSProperties;
     };
+    onMouseEnter?: () => void;
+    onMouseLeave?: () => void;
+    onClick?: () => void;
   }
 
-  export const ComposableMap: React.FC<{ projection?: string }>;
-  export const Geographies: React.FC<{ geography?: string }>;
+  export interface MarkerProps {
+    coordinates: [number, number];
+    children?: React.ReactNode;
+  }
+
+  export const ComposableMap: React.FC<{
+    projection?: string | object;
+    width?: number;
+    height?: number;
+  }>;
+
+  export const Geographies: React.FC<{
+    geography: string | GeoJsonObject | undefined;
+    children?: (props: { geographies: GeoJsonObject[] }) => React.ReactNode;
+  }>;
+
   export const Geography: React.FC<GeographyProps>;
-  export const Marker: React.FC<{ coordinates: [number, number] }>;
-  export const ZoomableGroup: React.FC;
+  export const Marker: React.FC<MarkerProps>;
+  export const ZoomableGroup: React.FC<{
+    center?: [number, number];
+    zoom?: number;
+    children?: React.ReactNode;
+  }>;
 }
