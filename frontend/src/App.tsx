35e4219d22e99e61412c943506ae3922dd12487d
kishlay <kishlay141@gmail.com>
2025-06-05 11:19:59 +0530

    renamed the file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello Tailwind!
-      </h1>
-    </div>
-  );
-}
-export default App;
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { PluginAnalysis } from "./pages/PluginAnalysis";
+import { InterlinkAnalysis } from "./pages/InterlinkAnalysis";
+import { CyberLoader } from "./components/UI/CyberLoader";
