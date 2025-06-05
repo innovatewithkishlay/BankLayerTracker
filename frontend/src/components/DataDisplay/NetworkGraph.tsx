3ddac93395c8ca9d64bf34ceac37ad7b6182d060
kishlay <kishlay141@gmail.com>
2025-06-05 10:39:11 +0530

    added Network GraphProps

--- frontend/src/components/DataDisplay/NetworkGraph.tsx
+++ frontend/src/components/DataDisplay/NetworkGraph.tsx
@@ -1,3 +1,8 @@
 import ReactFlow, { Background, Controls } from "reactflow";
 import type { Edge, Node } from "reactflow";
 import "reactflow/dist/style.css";
+
+interface NetworkGraphProps {
+  nodes: Node[];
+  edges: Edge[];
+}
