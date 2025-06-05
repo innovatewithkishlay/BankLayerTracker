4af96c466d8bfdc84ad1d304df0ac70169b35938
kishlay <kishlay141@gmail.com>
2025-06-05 11:04:45 +0530

    started with PluginAnalysis

---
+++ frontend/src/pages/PluginAnalysis.tsx
@@ -0,0 +1,5 @@
+import { useApi } from "../hooks/useApi";
+import { FileUpload } from "../components/Input/FileUpload";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
