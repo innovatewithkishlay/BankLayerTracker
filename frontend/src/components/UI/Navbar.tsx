c60ac2282f181e3dd5a9de9ac3313ae6eb91c04e
kishlay <kishlay141@gmail.com>
2025-06-06 21:56:26 +0530

    started with the navbar

---
+++ frontend/src/components/UI/Navbar.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { motion, AnimatePresence } from "framer-motion";
+import { FiUser, FiLogOut, FiTerminal } from "react-icons/fi";
+import { useClickOutside } from "../../hooks/useClickOutside";
+import { useNavigate } from "react-router-dom";
