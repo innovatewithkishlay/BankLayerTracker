d5518153035e2e90f0e185969e87576033e6354f
kishlay <kishlay141@gmail.com>
2025-06-06 11:58:02 +0530

    made User schema

--- backend/app/models/User.js
+++ backend/app/models/User.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  googleId: String,
+  email: String,
+  name: String,
+  avatar: String,
+  role: { type: String, default: "user" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("User", userSchema);
