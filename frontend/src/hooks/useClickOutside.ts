6e5df275ca469d8ec18e95f430fe27f3f39c6891
kishlay <kishlay141@gmail.com>
2025-06-06 17:28:59 +0530

    updated the  useClickOutside.ts

--- frontend/src/hooks/useClickOutside.ts
+++ frontend/src/hooks/useClickOutside.ts
@@ -1,1 +1,18 @@
 import { useEffect, useRef } from "react";
+
+export const useClickOutside = (handler: () => void) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handler();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClick);
+    return () => document.removeEventListener("mousedown", handleClick);
+  }, [handler]);
+
+  return ref;
+};
