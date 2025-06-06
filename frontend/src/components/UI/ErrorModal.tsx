5ce1626916cb49776f424319a1dedd25a7145629
kishlay <kishlay141@gmail.com>
2025-06-06 17:46:05 +0530

    made ErrorMOdalProps

--- frontend/src/components/UI/ErrorModal.tsx
+++ frontend/src/components/UI/ErrorModal.tsx
@@ -1,2 +1,9 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { FiAlertTriangle, FiX } from "react-icons/fi";
+interface ErrorModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+  details?: string[];
+}
