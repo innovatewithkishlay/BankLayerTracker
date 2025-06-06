de1e26b73357df336a0a9e56e8600a5f096bc94d
kishlay <kishlay141@gmail.com>
2025-06-06 13:47:25 +0530

    removed the cyberLoader from the App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,13 @@
-// import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { PluginAnalysis } from "./pages/PluginAnalysis";
 import { InterlinkAnalysis } from "./pages/InterlinkAnalysis";
 import { Results } from "./pages/Results";
 import { CompareResults } from "./pages/CompareResults";
-// import { CyberLoader } from "./components/UI/CyberLoader";
 
 export const App = () => {
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [progress, setProgress] = useState(0);
-
-  // useState(() => {
-  //   const interval = setInterval(() => {
-  //     setProgress((prev) => {
-  //       if (prev >= 100) {
-  //         clearInterval(interval);
-  //         setIsLoading(false);
-  //         return 100;
-  //       }
-  //       return prev + 1;
-  //     });
-  //   }, 50);
-  // });
-
   return (
     <Router>
-      {/* {isLoading ? (
-        <CyberLoader progress={progress} />
-      ) : ( */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/plugin" element={<PluginAnalysis />} />
@@ -39,7 +18,6 @@ export const App = () => {
           element={<CompareResults />}
         />
       </Routes>
-      {/* // )} */}
     </Router>
   );
 };
