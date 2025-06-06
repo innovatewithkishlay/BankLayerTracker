1e70430b96a667be50abe672aa4de9fd7f6e80d1
kishlay <kishlay141@gmail.com>
2025-06-06 17:46:20 +0530

    gave a line

--- frontend/src/components/UI/ErrorModal.tsx
+++ frontend/src/components/UI/ErrorModal.tsx
@@ -1,5 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { FiAlertTriangle, FiX } from "react-icons/fi";
+
 interface ErrorModalProps {
   isOpen: boolean;
   onClose: () => void;
