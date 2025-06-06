573cced22f8acce9101e2dbd509ed0eef0d0918c
kishlay <kishlay141@gmail.com>
2025-06-06 14:01:34 +0530

    declared the user type

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,3 +1,9 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+
+type User = {
+  email: string;
+  name: string;
+  avatar?: string;
+};
