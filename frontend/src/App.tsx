9d91e36b265c48b4ba5eed6a70ef76305e8aff65
kishlay <kishlay141@gmail.com>
2025-06-08 14:40:24 +0530

    created Route in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,6 +24,7 @@ export const App = () => {
           />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/pro" element={<ProButtonDemo />} />
+          <Route path="/contribute" element={<ContributePage />} />
         </Routes>
 
         <CyberToast />
